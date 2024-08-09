4990e02a2fe24938392b6150fdb29a7ad79e4acf
Liam Davis <davisliam123@gmail.com>
2024-08-09 14:45:27 -0700

    updated dependencies

--- stock_tracker_web_app/settings.py
+++ stock_tracker_web_app/settings.py
@@ -16,10 +16,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# heroku settings
-import django_heroku
-django_heroku.settings(locals())
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
