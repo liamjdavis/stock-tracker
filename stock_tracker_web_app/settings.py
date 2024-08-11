390621e00a8d8d721d9265e03083d3cfdce15ee0
Liam Davis <davisliam123@gmail.com>
2024-08-11 16:41:26 -0700

    deploy app

--- stock_tracker_web_app/settings.py
+++ stock_tracker_web_app/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'stock_tracker_web_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
