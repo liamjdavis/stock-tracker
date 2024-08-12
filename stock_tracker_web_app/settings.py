58c7fcd05c1d8c2741336298e71759d66d60d545
Liam Davis <davisliam123@gmail.com>
2024-08-11 17:04:17 -0700

    add allowed host

--- stock_tracker_web_app/settings.py
+++ stock_tracker_web_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-m$0wzkgai7_=5$w%=1-cp!tn3v(h*d(db^f9i(u)-#@g@m&re!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i2i-stock-scraper-229ccec1ddfa.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['i2i-stock-scraper-3f03113a80a2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
