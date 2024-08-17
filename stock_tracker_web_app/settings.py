4b6e929d0a62d6c77df4090630aa6f21cd3d41bd
Liam Davis <davisliam123@gmail.com>
2024-08-17 14:47:46 -0700

    migrated database to heroku

--- stock_tracker_web_app/settings.py
+++ stock_tracker_web_app/settings.py
@@ -84,10 +84,17 @@ WSGI_APPLICATION = 'stock_tracker_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_postgres',
+        'USER': 'liam',
+        'PASSWORD': 'Ih8thepirates',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
